db4c74a4968ebad1b076b9753e372dc82a9edb06
P Nagendra Kumar <pnagendra.kumar@rci.rogers.com>
2019-11-22 12:58:25 +0530

    [YE-Mario] Publishing Build Folder

---
+++ build/precache-manifest.dbf4695f0adc53724b06caaec3cde797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4880df6f31a5fe15b18931ae8f52d15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a761c6df081442cb0973",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "df5fb8ea5cb968c7075e",
+    "url": "/static/css/main.c406ac7f.chunk.css"
+  },
+  {
+    "revision": "a761c6df081442cb0973",
+    "url": "/static/js/2.27635329.chunk.js"
+  },
+  {
+    "revision": "df5fb8ea5cb968c7075e",
+    "url": "/static/js/main.47a752bf.chunk.js"
+  },
+  {
+    "revision": "6c5be4f819d1efa9332c",
+    "url": "/static/js/runtime-main.56caa9e6.js"
+  }
+]);
